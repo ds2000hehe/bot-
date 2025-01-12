1f80ce6c42f7e1ae4036964c90288925e5f79a1a
David <david@UbuntuRos.myguest.virtualbox.org>
2025-01-12 00:51:47 +0000

    Committer: David <david@UbuntuRos.myguest.virtualbox.org>  On branch main 	new file:   config/my_controllers.yaml 	modified:   description/robot.urdf.xacro 	new file:   description/ros2_control.xacro 	modified:   launch/launch_sim.launch.py

--- launch/launch_sim.launch.py
+++ launch/launch_sim.launch.py
@@ -7,6 +7,7 @@ from launch import LaunchDescription
 from launch.actions import IncludeLaunchDescription
 from launch.launch_description_sources import PythonLaunchDescriptionSource
 
+
 from launch_ros.actions import Node
 
 
@@ -34,13 +35,27 @@ def generate_launch_description():
     # Run the spawner node from the gazebo_ros package. The entity name doesn't really matter if you only have a single robot.
     spawn_entity = Node(package='gazebo_ros', executable='spawn_entity.py',
                         arguments=['-topic', 'robot_description',
-                                   '-entity', 'my_bot'],
+                                   '-entity', 'bot-'],
                         output='screen')
+
+    diff_drive_spawner = Node(
+        package="controller_manager",
+        executable="spawner.py",
+        arguments=["diff_cont"],
+    )
+    joint_broad_spawner = Node(
+        package="controller_manager",
+        executable="spawner.py",
+        arguments=["joint_broad"],
+    )
+
 
 # Launch them all!
     return LaunchDescription([
         rsp,
         gazebo,
         spawn_entity,
+        diff_drive_spawner,
+        joint_broad_spawner,
     ])
 
